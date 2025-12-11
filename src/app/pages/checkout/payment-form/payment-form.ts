c1f0bb2eeb771094d6dbd81146e62d81df6fc46e
Matt <solomatias321@gmail.com>
2025-12-11 18:30:23 +0100

    feat: adding checkout page

--- src/app/pages/checkout/payment-form/payment-form.ts
+++ src/app/pages/checkout/payment-form/payment-form.ts
@@ -1,18 +1,23 @@
 import { MatIcon } from '@angular/material/icon';
 import { Component } from '@angular/core';
 import { ViewPanel } from '../../../directives/view-panel.directive';
+import { MatRadioGroup, MatRadioButton } from '@angular/material/radio';
 
 @Component({
   selector: 'app-payment-form',
-  imports: [ViewPanel, MatIcon],
+  imports: [ViewPanel, MatIcon, MatRadioGroup, MatRadioButton],
   template: `
     <div appViewPanel>
       <h2 class="text-2xl font-bold mb-6 flex items-center gap-2">
         <mat-icon class="text-gray-400 text-6xl">payment</mat-icon>
-        Payment Information
+        Payment Optoins
       </h2>
       <div>
-        <mat-radio-group> </mat-radio-group>
+        <mat-radio-group [value]="'stripe'">
+          <mat-radio-button class="block mb-4" value="stripe">
+            <img src="stripe-logo.png" alt="stripe" class="h-8 inline" />
+          </mat-radio-button>
+        </mat-radio-group>
       </div>
     </div>
   `,
