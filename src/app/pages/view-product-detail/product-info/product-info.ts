eed3a4057e7a6f6a0a206da2ed820fae488b138c
Matt <solomatias321@gmail.com>
2025-12-20 13:57:05 +0100

    fix: add product info

--- src/app/pages/view-product-detail/product-info/product-info.ts
+++ src/app/pages/view-product-detail/product-info/product-info.ts
@@ -1,12 +1,62 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { Product } from '../../../models/product';
+import { TitleCasePipe } from '@angular/common';
+import { StockStatus } from '../stock-status/stock-status';
+import { QtySelectorComponent } from '../../../components/qty-selector/qty-selector.component';
+import { MatAnchor } from '@angular/material/button';
+import { EcommerceStore } from '../../../ecommerce-store';
+import { MatIcon } from '@angular/material/icon';
+import { ToggleWishlistButtonComponent } from '../../../components/toggle-wishlist-button/toggle-wishlist-button';
 
 @Component({
   selector: 'app-product-info',
-  imports: [],
-  template: ` <p>product-info works!</p> `,
+  imports: [
+    TitleCasePipe,
+    StockStatus,
+    QtySelectorComponent,
+    MatAnchor,
+    MatIcon,
+    ToggleWishlistButtonComponent,
+  ],
+  template: `
+    <div class="text-xs rounded-xl bg-gray-100 px-2 py-1 w-fit mb-2">
+      {{ product().category | titlecase }}
+    </div>
+    <h1 class="text-2xl font-extrabold mb-3">{{ product().name }}</h1>
+    <p class="text-3xl font-extrabold mb-4">\${{ product().price }}</p>
+    <app-stock-status [inStock]="product().inStock" />
+    <p class="font-semibold mb-2">Description</p>
+    <p class="text-gray-600 border-b border-gray-200 pb-4">
+      {{ product().description }}
+    </p>
+    <div class="flex items-center gap-2 mb-3 pt-4">
+      <span class="font-semibold">Quantity:</span>
+      <app-qty-selector
+        [quantity]="quantity()"
+        (qtyUpdated)="quantity.set($event)"
+      />
+    </div>
+    <div class="flex gap-4 mb border-b border-gray-200 pb-4">
+      <button
+        matButton="filled"
+        class="w-2/3 flex items-center gap-2 "
+        (click)="store.addToCart(product(), quantity())"
+        [disabled]="!product().inStock"
+      >
+        <mat-icon>shopping_cart</mat-icon>
+        {{ product().inStock ? 'Add to Cart' : 'Out of Stock' }}
+      </button>
+      <app-toggle-wishlist-button [product]="product()" />
+      <button matIconButton>
+        <mat-icon>share</mat-icon>
+      </button>
+    </div>
+  `,
   styles: ``,
 })
 export class ProductInfo {
   product = input.required<Product>();
+
+  quantity = signal(1);
+  store = inject(EcommerceStore);
 }
